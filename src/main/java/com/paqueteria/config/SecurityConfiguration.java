4a23ab5943d1f99b39468970e07bfc705fd9a383
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2026-01-16 19:48:51 +0100

    added configuracion para las rutas

--- src/main/java/com/paqueteria/config/SecurityConfiguration.java
+++ src/main/java/com/paqueteria/config/SecurityConfiguration.java
@@ -1,12 +1,29 @@
 package com.paqueteria.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
-@Bean
-public static BCryptPasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
+@   Bean
+    public static BCryptPasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/auth/registro", "/css/**", "/js/**").permitAll()  // Permite acceso sin login
+                        .anyRequest().authenticated()  // El resto requiere autenticación
+                )
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
 }
