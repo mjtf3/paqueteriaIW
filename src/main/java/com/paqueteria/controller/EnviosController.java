f27acead184bebcae54aaf11baa3ac84960b0561
Manuel José Tornero Fuster <91553219+mjtf3@users.noreply.github.com>
2026-01-17 18:05:12 +0100

    fix dtos (#30)
    * fix dtos
    
    * fix
    
    * bien

--- src/main/java/com/paqueteria/controller/EnviosController.java
+++ src/main/java/com/paqueteria/controller/EnviosController.java
@@ -1,10 +1,6 @@
 package com.paqueteria.controller;
 
-import com.paqueteria.dto.CrearEnvioDTO;
-import com.paqueteria.dto.EnvioDTO;
-import com.paqueteria.security.RequireApiKey;
-import com.paqueteria.service.EnvioService;
-import jakarta.validation.Valid;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paqueteria.dto.CrearEnvioDTO;
+import com.paqueteria.dto.EnvioDTO;
+import com.paqueteria.security.RequireApiKey;
+import com.paqueteria.service.EnvioService;
+
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/envios")
+@RegisterReflectionForBinding({CrearEnvioDTO.class, EnvioDTO.class})
 public class EnviosController {
 
     @Autowired
@@ -25,6 +29,6 @@ public class EnviosController {
     @ResponseStatus(HttpStatus.CREATED)
     public EnvioDTO crearEnvio(@Valid @RequestBody CrearEnvioDTO dto) {
         // Cuando tengamos usuarios re tiene que pillas la api key, un servicio get id by api key y pasarla aqui
-        return envioService.crearEnvio(dto, 1); // TODO: Usuario simulado con ID 1; obtener ID de usuario autenticado
+        return envioService.crearEnvio(dto, -1); // TODO: Usuario simulado con ID -1; obtener ID de usuario autenticado
     }
 }
