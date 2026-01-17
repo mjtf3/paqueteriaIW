fd3251d8a98207588378271fa4d8fc1db82d123e
Carlos Maria Casado López <106508091+CcWhyNot@users.noreply.github.com>
2026-01-17 13:41:17 +0100

    11 registro e inicio de sesion de tienda (#26)
    * Added dependencia para hashear password
    
    * Creado dto para usuario
    
    * added equal y hascode
    
    * corregido error de import
    
    * configurado modelmapper
    
    * added configuracion para las rutas
    
    * creada template de registro
    
    * added funcion para buscar por correo
    
    * Modelo actualizado de usuario
    
    * registro de tiendas
    
    * added template de loginform y bypass de la ruta y controlador
    
    * fix deploy
    
    * Configurado login mediante la dependencia
    
    * templates de registro y login
    
    * funcion registrar y findemail y gestion de redireccion de usuarios
    
    * no se ha subido que ya no hay funcion de login en el controlador
    
    * pr fix
    
    * manuel requisitos de airear
    
    * quitado debug de login
    
    * usuario -> correo
    
    ---------
    
    Co-authored-by: Manuel Tornero <mjtf3@alu.ua.es>

---
+++ src/main/java/com/paqueteria/services/UsuarioService.java
@@ -0,0 +1,53 @@
+package com.paqueteria.services;
+
+import com.paqueteria.dto.UsuarioData;
+import com.paqueteria.model.Usuario;
+import com.paqueteria.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public UsuarioData registrar(UsuarioData usuario) {
+        Optional<Usuario> usuarioBD = usuarioRepository.findByCorreo(usuario.getCorreo());
+        if (usuarioBD.isPresent()) {
+            throw new UsuarioServiceException("El usuario " + usuario.getCorreo() + " ya existe");
+        }
+        else if(usuario.getCorreo() == null){
+            throw new UsuarioServiceException("El correo no puede ser nulo");
+        } else if (usuario.getContrasena() == null) {
+            throw new UsuarioServiceException("La contrasena no puede ser nula");
+        }
+        else {
+            Usuario usuarioNuevo = modelMapper.map(usuario,Usuario.class);
+            usuarioNuevo.setContrasena(passwordEncoder.encode(usuario.getContrasena()));
+            usuarioRepository.save(usuarioNuevo);
+
+            return modelMapper.map(usuarioNuevo,UsuarioData.class);
+        }
+    }
+
+    @Transactional
+    public UsuarioData findByCorreo(String correo) {
+        Usuario usuarioBD = usuarioRepository.findByCorreo(correo).orElse(null);
+        if (usuarioBD == null) {return null;}
+        return modelMapper.map(usuarioBD,UsuarioData.class);
+    }
+}
