60df82729ab83d150df41647faf033dfa89acc97
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2026-01-17 13:26:09 +0100

    manuel requisitos de airear

--- src/main/java/com/paqueteria/dto/UsuarioData.java
+++ src/main/java/com/paqueteria/dto/UsuarioData.java
@@ -4,6 +4,7 @@ import com.paqueteria.model.TipoEnum;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
+
 import java.util.Objects;
 
 
@@ -26,30 +27,101 @@ public class UsuarioData {
     private BigDecimal pesoMaximo;
     private Boolean activa = true;
 
-    public Integer getId() {return id;}
-    public void setId(Integer id) {this.id = id;}
-    public String getApodo() {return apodo;}
-    public void setApodo(String apodo) {this.apodo = apodo;}
-    public String getNombre() {return nombre;}
-    public void setNombre(String nombre) {this.nombre = nombre;}
-    public String getNombreTienda() {return nombreTienda;}
-    public void setNombreTienda(String nombreTienda) {this.nombreTienda = nombreTienda;}
-    public String getApellidos() {return apellidos;}
-    public void setApellidos(String apellidos) {this.apellidos = apellidos;}
-    public TipoEnum getTipo() {return tipo;}
-    public void setTipo(TipoEnum tipo) {this.tipo = tipo;}
-    public String getCorreo() {return correo;}
-    public void setCorreo(String correo) {this.correo = correo;}
-    public String getTelefono() {return telefono;}
-    public void setTelefono(String telefono) {this.telefono = telefono;}
-    public String getContrasena() {return contrasena;}
-    public void setContrasena(String contrasena) {this.contrasena = contrasena;}
-    public LocalDate getFechaCreacion() {return fechaCreacion;}
-    public void setFechaCreacion(LocalDate fechaCreacion) {this.fechaCreacion = fechaCreacion;}
-    public BigDecimal getPesoMaximo() {return pesoMaximo;}
-    public void setPesoMaximo(BigDecimal pesoMaximo) {this.pesoMaximo = pesoMaximo;}
-    public Boolean getActiva() {return activa;}
-    public void setActiva(Boolean activa) {this.activa = activa;}
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreTienda() {
+        return nombreTienda;
+    }
+
+    public void setNombreTienda(String nombreTienda) {
+        this.nombreTienda = nombreTienda;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public TipoEnum getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEnum tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public BigDecimal getPesoMaximo() {
+        return pesoMaximo;
+    }
+
+    public void setPesoMaximo(BigDecimal pesoMaximo) {
+        this.pesoMaximo = pesoMaximo;
+    }
+
+    public Boolean getActiva() {
+        return activa;
+    }
+
+    public void setActiva(Boolean activa) {
+        this.activa = activa;
+    }
 
     @Override
     public boolean equals(Object o) {
